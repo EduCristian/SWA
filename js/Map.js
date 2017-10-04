864fee468d7f850f42fbdb4f3bd28a7be91e7b4d
Rosalie Peree <rosalie.peree@etu.univ-lyon1.fr>
2017-10-04 15:02:30 +0200

    add map and reduce (Chris)

---
+++ js/Map.js
@@ -0,0 +1,68 @@
+var unorderList = document.getElementById('unorderList');
+
+var numbers = [];
+var halvedNumbers = [];
+
+function getNumbers(array){
+    var number = '';
+    for(var i = 0; i < array.length; i++){
+        for(var j = 0; j < array[i].textContent.length; j++){
+            if(  array[i].textContent.charAt(j) == '0' || array[i].textContent.charAt(j) == '1'
+                || array[i].textContent.charAt(j) == '2' || array[i].textContent.charAt(j) == '3'
+                || array[i].textContent.charAt(j) == '4' || array[i].textContent.charAt(j) == '5'
+                || array[i].textContent.charAt(j) == '6' || array[i].textContent.charAt(j) == '7'
+                || array[i].textContent.charAt(j) == '8' || array[i].textContent.charAt(j) == '9'){
+                number += array[i].textContent.charAt(j);
+            }
+        }
+
+        numbers.push(parseInt(number));
+        number = '';
+    }
+    return numbers;
+}
+
+function getHalvedNumbers(numberArray){
+    halvedNumbers = numbers.map(function(num){return num / 2;})
+}
+
+
+function replaceWithNewNumbers(oldArray){
+
+    var newArray = [];
+    var dotsPosition = 0;
+
+
+
+
+    for(var i = 0; i < oldArray.length; i++)
+    {
+        for(var j = 0; j < oldArray[0].textContent.length; j++){
+            if(oldArray[i].textContent.charAt(j) == ':'){
+                j++;
+                newArray[i] = oldArray[i].textContent.slice(0, j) + " " + halvedNumbers[i] + oldArray[i].textContent.slice(j + 5);
+                console.log(oldArray[i].textContent.slice(0, j));
+                console.log(oldArray[i].textContent.slice(j + 5));
+            }
+        }
+    }
+
+    return newArray;
+}
+
+
+function salesOf()
+{
+    getNumbers(unorderList.children);
+    getHalvedNumbers(numbers);
+    var newList = replaceWithNewNumbers(unorderList.children);
+
+    x = document.getElementById("saleP");
+    x.innerHTML = newList;
+}
+
+salesOf();
+
+var shopItem.Prototype = {
+    name
+}
---
+++ js/Reduce.js
@@ -0,0 +1,34 @@
+var unorderList = document.getElementById('unorderList');
+
+function getNumbers(array){
+    var numbers = [];
+    var number = '';
+    for(var i = 0; i < array.length; i++){
+        for(var j = 0; j < array[i].textContent.length; j++){
+            if(  array[i].textContent.charAt(j) == '0' || array[i].textContent.charAt(j) == '1' 
+              || array[i].textContent.charAt(j) == '2' || array[i].textContent.charAt(j) == '3'
+              || array[i].textContent.charAt(j) == '4' || array[i].textContent.charAt(j) == '5'
+              || array[i].textContent.charAt(j) == '6' || array[i].textContent.charAt(j) == '7'
+              || array[i].textContent.charAt(j) == '8' || array[i].textContent.charAt(j) == '9'){
+                    number += array[i].textContent.charAt(j);    
+            }
+        }
+            
+        numbers.push(parseInt(number));
+        number = '';
+    }
+    return numbers;
+}
+
+function average(numberArray){
+    var avg = 0;
+    
+  
+    avg = numberArray.reduce(function (sum, number) {
+  return sum + number;
+}, 0) / numberArray.length;
+return avg;
+}
+
+document.createElement('label1')
+document.getElementById('label').innerHTML = average(getNumbers(unorderList.children));
