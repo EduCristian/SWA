3d9480ce17a5f137832c968098e7040099c5732c
danielJey <daniel_jey95@outlook.com>
2017-10-09 14:41:46 +0200

    MAde create objects

--- js/filter.js
+++ js/filter.js
@@ -1,7 +1,6 @@
 function sliderChange(val)
 {
 	document.getElementById('sliderStatus').innerHTML = val;
-	filterSaddle(val)
 }
 
 function saddleFilter() 
@@ -22,6 +21,47 @@ function saddleFilter()
 			li[i].style.display = "none";
 		}
 	}
+}
+
+
+function saddle(name, price, size, colour) 
+{
+	this.name = name;
+	this.price = price;
+	this.size = size;
+	this.colour = colour;
+}
+
+var saddle1 = new saddle("Leather saddle", 50, "medium", "blue");
+var saddle2 = new saddle("Custom saddle", 70, "medium", "green");
+var saddle3 = new saddle("Racing saddle", 100, "large", "black");
+var saddle4 = new saddle("City saddle", 80, "small", "red");
+var saddle5 = new saddle("Brooks saddle", 60, "large", "brown");
+var saddle6 = new saddle("Suede saddle", 90, "medium", "black");
+var saddle7 = new saddle("Biker saddle", 30, "medium", "navy");
+var saddle8 = new saddle("Children saddle", 40, "small", "green");
+
+var saddleList =
+[
+	saddle1, saddle2, saddle3, saddle4, saddle5, saddle6, saddle7, saddle8
+];
+
+function printArr(a)
+{
+	for(var i = 0; i <= a.length-1; i++)
+	{
+		console.log(a[i])
+	}
+}
+function filterByPrice(item)
+{
+	if(item.price >= 80){return true;}
+	return false;
+}
+
+function filteredList(list)
+{
+	saddleList = list.filter(filterByPrice);
 }
 
 saddleFilter();
