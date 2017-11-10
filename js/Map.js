05da9be7d01c7743102e4d666e6abd253eb5f7ef
Cristian <educristian17@gmail.com>
2017-11-10 13:02:12 +0100

    changed the method to use Map instead of 2 loops

--- js/Map.js
+++ js/Map.js
@@ -3,20 +3,53 @@ var unorderList = document.getElementById('unorderList');
 var numbers = [];
 var halvedNumbers = [];
 
-function getNumbers(array) {
-    var number = '';
-    for (var i = 0; i < array.length; i++){
-        for (var j = 0; j < array[i].textContent.length; j++){
-            if(  array[i].textContent.charAt(j) >= '0' && array[i].textContent.charAt(j) <= '9'){
-                number += array[i].textContent.charAt(j);
+function getNumbers(HTMLcollection) 
+{
+    
+    // for (var i = 0; i < array.length; i++){
+    //     for (var j = 0; j < array[i].textContent.length; j++)
+    //     {
+    //         if(  array[i].textContent.charAt(j) >= '0' && array[i].textContent.charAt(j) <= '9')
+    //         {
+    //             number += array[i].textContent.charAt(j);
+    //         }
+    //     }
+
+
+    //     numbers.push(parseInt(number));
+    //     number = '';
+    // }
+    var CollectiontoArray = Array.from(HTMLcollection)
+
+    numbers = CollectiontoArray.map(function(element){
+        var number = '';
+        for (var j = 0; j < CollectiontoArray.length; j++)
+        {
+            if(CollectiontoArray[j] >= '0' && CollectiontoArray[j] <= '9')
+            {
+                number += CollectiontoArray[j];
             }
         }
 
-        numbers.push(parseInt(number));
-        number = '';
-    }
-    return numbers;
+        return number;
+    })
+
+    //return numbers;
 }
+
+function collectNumberFromString(string){
+        var number = '';
+        for (var j = 0; j < string[i].textContent.length; j++)
+        {
+            if(  string[i].textContent.charAt(j) >= '0' && string[i].textContent.charAt(j) <= '9')
+            {
+                number += string[i].textContent.charAt(j);
+            }
+        }
+        var intNumber = parseInt(number);
+        console(intNumber);
+        return intNumber;
+    }
 
 function getHalvedNumbers(numberArray){
     halvedNumbers = numbers.map(function(num){return num / 2;})
@@ -27,9 +60,6 @@ function replaceWithNewNumbers(oldArray){
 
     var newArray = [];
     var dotsPosition = 0;
-
-
-
 
     for(var i = 0; i < oldArray.length; i++)
     {
@@ -59,6 +89,4 @@ function salesOf()
 
 salesOf();
 
-var shopItem.Prototype = {
-    name
-}
+// var shopItem.Prototype = {}
