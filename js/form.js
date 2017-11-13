bf45bd2ea546523084fb17a219220970cca87c02
RosaliePeree <rosalie.peree@etu.univ-lyon1.fr>
2017-11-13 14:17:10 +0100

    update on the form, using customvalidity instead of windows alert

--- js/form.js
+++ js/form.js
@@ -4,68 +4,69 @@ function validation(){
     var email = document.getElementById("input_email");
     var subject = document.getElementById("input_subject");
     var messag = document.getElementById("input_message");
+    
     if (name.value === ""){
-        window.alert("Please enter your name.");
+        name.setCustomValidity("Please enter your name.");
+        console.log("Check the name");
         name.focus();
-        return false;
+    } else {
+        name.setCustomValidity('');
     }
     
     if (email.value === ""){
-        window.alert("Please enter an e-mail address.");
+        email.setCustomValidity("Please enter an e-mail address.");
+                console.log("Check the email");
         email.focus();
-        return false;
+    } else {
+        email.setCustomValidity('');
     }
     
     // The email address is not tested with regex because it is just a security, but the only way to make sure
     // the email address is correct is sending an email. Therefore, we decided to just make a very simple check
     // instead of a regex check (like the one using the RCF 2822 standard).
     if (email.value.indexOf("@", 0) < 0){
-        window.alert("Please enter a valid e-mail address.");
+        email.setCustomValidity("Please enter a valid e-mail address.");
         email.focus();
-        return false;
+    } else {
+        email.setCustomValidity('');
     }
     
     if (email.value.indexOf(".", 0) < 0){
-        window.alert("Please enter a valid e-mail address.");
+        email.setCustomValidity("Please enter a valid e-mail address.");
         email.focus();
-        return false;
+    } else {
+        email.setCustomValidity('');
     }
     
     if (subject.value === ""){
-        window.alert("Please enter a subject for your enquiry.")
+        subject.setCustomValidity("Please enter a subject for your enquiry.")
         subject.focus();
-        return false;
+    } else {
+        subject.setCustomValidity('');
     }
     
     if (messag.value === "") {
-        window.alert("Please enter a message!");
+        messag.setCustomValidity("Please enter a message!");
         messag.focus();
-        return false;
+    } else {
+        messag.setCustomValidity('');
     }
-    return true;
 }
 
-window.onload=function(){
-    document.getElementById('id_form').addEventListener("submit", function(e)
-    {
-            event.preventDefault();
-            console.log("call validate ");
-            ValidateForm();
-        console.log("validated");
-            //write stuff
-    });
-}
+var form = document.getElementById('id_form');
+var submitButton = document.getElementById('input-submit');
+
+submitButton.onclick = function () { ValidateForm(); };
 
 /* Function to validate the whole form. Displays a confirmation message if the validation function (line 1) returns true */
 function ValidateForm(){
-        var name = document.getElementById("input_name").value;
-        var email = document.getElementById("input_email").value;
             console.log("hey");
-        if (validation()) // Calling validation function
-        {
+        var valid = validation(); // Calling validation function
+
             console.log("hello");
             //document.getElementById("id_form").submit(); //form submission that we don't need for now
-            window.confirm(" Name: " + name + " \n Email: " + email + " \n Subject: " + document.getElementById("input_subject").value + " \n Message: " + document.getElementById("input_message").value + "\n\n Confirm the submission");
-        }
-    
+            window.confirm(" Name: " + document.getElementById("input_name").value 
+                           + " \n Email: " + document.getElementById("input_email").value 
+                           + " \n Subject: " + document.getElementById("input_subject").value 
+                           + " \n Message: " + document.getElementById("input_message").value + "\n\n Confirm the submission");
 }
